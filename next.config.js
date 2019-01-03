0e876138e1460e1e75f52dfa4138fc236881f596
Tom Philbin <tomphilbin@users.noreply.github.com>
2019-01-03 21:57:49 +0000

    Bump to now v2

--- next.config.js
+++ next.config.js
@@ -1,3 +1,16 @@
-const withTypescript = require('@zeit/next-typescript');
+const { PHASE_PRODUCTION_SERVER } =
+    process.env.NODE_ENV === 'development'
+        ? {}
+        : !process.env.NOW_REGION
+        ? require('next/constants') // Get values from `next` package when building locally
+        : require('next-server/constants') // Get values from `next-server` package when building on now v2
 
-module.exports = withTypescript();
+module.exports = (phase, { defaultConfig }) => {
+    if (phase !== PHASE_PRODUCTION_SERVER) {
+        const withTypescript = require('@zeit/next-typescript')
+
+        return withTypescript()
+    }
+
+    return {}
+}
